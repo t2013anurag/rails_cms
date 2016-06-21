c073c9e3a7d56766683698dcd8220cb67bca6573
Anurag Tiwari <tiwari.anurag126@gmail.com>
2016-06-22 01:53:24 +0530

    Learn and learn

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -5,13 +5,19 @@ class DemoController < ApplicationController
   def index
   	# render hello template 
   	# render('demo/hello') same as render('hello') as hello is present in demo template views
-  	render('hello')
+  	# render('hello')
+
+
   end
 
   def hello
   	# render('index')
   	@arr = [1,2,3,4,5] # @ declared variables are instance variables, so that they cn be used in templates (views)
-  	puts "Hello"
+  	# puts "Hello"
+  	@id = params['id'].to_i
+  	@page = params[:page].to_i
+  	# both 'id' and :id are same in params
+  	# .to_i converts to integer
   end
 
   def other_hello
