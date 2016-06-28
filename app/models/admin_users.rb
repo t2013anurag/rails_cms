b9edb5b21c063b97f330d988add71a9241d2a3cb
Anurag Tiwari <tiwari.anurag126@gmail.com>
2016-06-29 00:33:29 +0530

    DB, queries and scopes

---
+++ app/models/admin_users.rb
@@ -0,0 +1,8 @@
+class AdminUser < ActiveRecord::Base
+
+
+# by default rails will assume table name as admin_users
+# we need to define the table that needs to be used here if we want a different table
+	# self.table_name = "users"
+
+end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,15 @@
 class Subject < ActiveRecord::Base
+
+	# definig scopes for shorter queries
+	scope :visible, lambda { where(visible: true) }
+	scope :invisible, lambda { where(visible: false) }
+	scope :sorted, lambda { order("subjects.position ASC") }
+	scope :newest_first, lambda { order("subjects.created_at DESC") }
+	scope :search, lambda { |query|  
+		where(["name LIKE ?", "%#{query}%"]) 
+	}
+	# scopes can be directly used as :  subject.visible or subject.visible.sorted or
+	# subject.find("ini").visible
+	
+
 end
